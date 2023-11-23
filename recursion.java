34bb6d430a59d0edd6f4fa09c4686a836052ed3f
pisethdy <dypiseth07@gmail.com>
2023-11-23 21:44:11 +0700

    fibonacci

--- recursion.java
+++ recursion.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 public class recursion {
-  public static void main(String[] args) {
-
-    //call your recursion fuction
-    //print result
-
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+    static int fib(int n) {
+        int a = 0, b = 1, c;
+        if (n == 0)
+            return a;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
     }
-  
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Fibonacci number at position " + n + ": " + fib(n));
+
+        scanner.close();
+    }
 }
     
